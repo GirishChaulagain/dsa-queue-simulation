de4b134c7f6675bc8d634f2965a47eed34bde8ba
Girish Chaulagain <girishchaulagain5@gmail.com>
2025-02-22 16:12:29 +0545

    updated CMakeList.txt and initlaized SDL

--- simulator/src/simulator.c
+++ simulator/src/simulator.c
@@ -1,1 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <SDL2/SDL.h>
+
+#define PORT 8080
+
+int InitializeSDL(void) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        SDL_Log("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return -1;
+    }
+    return 0;
+}
