bbe73440f06b77402712122b1e1ae80f4a879c85
Girish Chaulagain <girishchaulagain5@gmail.com>
2025-02-22 16:19:40 +0545

    fixed minor typo

---
+++ build/simulator/CMakeFiles/Simulator.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/girish/git/dsa-queue-simulator/simulator/src/simulator.c" "simulator/CMakeFiles/Simulator.dir/src/simulator.c.o" "gcc" "simulator/CMakeFiles/Simulator.dir/src/simulator.c.o.d"
+  "" "/home/girish/git/dsa-queue-simulator/bin/Simulator" "gcc" "simulator/CMakeFiles/Simulator.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
