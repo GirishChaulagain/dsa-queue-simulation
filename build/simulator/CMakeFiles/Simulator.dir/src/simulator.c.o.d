69ca7e153e58145326972b18ebb73ef37f1487b0
Girish Chaulagain <girishchaulagain5@gmail.com>
2025-02-22 17:37:44 +0545

    Initial setup of vehicle movement

--- build/simulator/CMakeFiles/Simulator.dir/src/simulator.c.o.d
+++ build/simulator/CMakeFiles/Simulator.dir/src/simulator.c.o.d
@@ -1,80 +1,83 @@
 simulator/CMakeFiles/Simulator.dir/src/simulator.c.o: \
  /home/girish/git/dsa-queue-simulator/simulator/src/simulator.c \
- /usr/include/stdc-predef.h /usr/include/stdio.h \
- /usr/include/bits/libc-header-start.h /usr/include/features.h \
- /usr/include/features-time64.h /usr/include/bits/wordsize.h \
- /usr/include/bits/timesize.h /usr/include/sys/cdefs.h \
- /usr/include/bits/long-double.h /usr/include/gnu/stubs.h \
- /usr/include/gnu/stubs-64.h \
+ /usr/include/stdc-predef.h /usr/include/SDL2/SDL_render.h \
+ /usr/include/SDL2/SDL_stdinc.h /usr/include/SDL2/SDL_config.h \
+ /usr/include/SDL2/SDL_platform.h /usr/include/SDL2/begin_code.h \
+ /usr/include/SDL2/close_code.h /usr/include/SDL2/SDL_config_unix.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/include/limits.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/include/syslimits.h \
+ /usr/include/limits.h /usr/include/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/bits/wordsize.h /usr/include/bits/timesize.h \
+ /usr/include/sys/cdefs.h /usr/include/bits/long-double.h \
+ /usr/include/gnu/stubs.h /usr/include/gnu/stubs-64.h \
+ /usr/include/bits/posix1_lim.h /usr/include/bits/local_lim.h \
+ /usr/include/linux/limits.h \
+ /usr/include/bits/pthread_stack_min-dynamic.h \
+ /usr/include/bits/pthread_stack_min.h /usr/include/bits/posix2_lim.h \
+ /usr/include/sys/types.h /usr/include/bits/types.h \
+ /usr/include/bits/typesizes.h /usr/include/bits/time64.h \
+ /usr/include/bits/types/clock_t.h /usr/include/bits/types/clockid_t.h \
+ /usr/include/bits/types/time_t.h /usr/include/bits/types/timer_t.h \
  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/include/stddef.h \
+ /usr/include/bits/stdint-intn.h /usr/include/endian.h \
+ /usr/include/bits/endian.h /usr/include/bits/endianness.h \
+ /usr/include/bits/byteswap.h /usr/include/bits/uintn-identity.h \
+ /usr/include/sys/select.h /usr/include/bits/select.h \
+ /usr/include/bits/types/sigset_t.h /usr/include/bits/types/__sigset_t.h \
+ /usr/include/bits/types/struct_timeval.h \
+ /usr/include/bits/types/struct_timespec.h \
+ /usr/include/bits/pthreadtypes.h /usr/include/bits/thread-shared-types.h \
+ /usr/include/bits/pthreadtypes-arch.h \
+ /usr/include/bits/atomic_wide_counter.h /usr/include/bits/struct_mutex.h \
+ /usr/include/bits/struct_rwlock.h /usr/include/stdio.h \
  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/include/stdarg.h \
- /usr/include/bits/types.h /usr/include/bits/typesizes.h \
- /usr/include/bits/time64.h /usr/include/bits/types/__fpos_t.h \
- /usr/include/bits/types/__mbstate_t.h \
+ /usr/include/bits/types/__fpos_t.h /usr/include/bits/types/__mbstate_t.h \
  /usr/include/bits/types/__fpos64_t.h /usr/include/bits/types/__FILE.h \
  /usr/include/bits/types/FILE.h /usr/include/bits/types/struct_FILE.h \
  /usr/include/bits/types/cookie_io_functions_t.h \
  /usr/include/bits/stdio_lim.h /usr/include/bits/floatn.h \
  /usr/include/bits/floatn-common.h /usr/include/stdlib.h \
  /usr/include/bits/waitflags.h /usr/include/bits/waitstatus.h \
- /usr/include/sys/types.h /usr/include/bits/types/clock_t.h \
- /usr/include/bits/types/clockid_t.h /usr/include/bits/types/time_t.h \
- /usr/include/bits/types/timer_t.h /usr/include/bits/stdint-intn.h \
- /usr/include/endian.h /usr/include/bits/endian.h \
- /usr/include/bits/endianness.h /usr/include/bits/byteswap.h \
- /usr/include/bits/uintn-identity.h /usr/include/sys/select.h \
- /usr/include/bits/select.h /usr/include/bits/types/sigset_t.h \
- /usr/include/bits/types/__sigset_t.h \
- /usr/include/bits/types/struct_timeval.h \
- /usr/include/bits/types/struct_timespec.h \
- /usr/include/bits/pthreadtypes.h /usr/include/bits/thread-shared-types.h \
- /usr/include/bits/pthreadtypes-arch.h \
- /usr/include/bits/atomic_wide_counter.h /usr/include/bits/struct_mutex.h \
- /usr/include/bits/struct_rwlock.h /usr/include/alloca.h \
- /usr/include/bits/stdlib-float.h /usr/include/string.h \
- /usr/include/bits/types/locale_t.h /usr/include/bits/types/__locale_t.h \
- /usr/include/strings.h /usr/include/unistd.h \
- /usr/include/bits/posix_opt.h /usr/include/bits/environments.h \
- /usr/include/bits/confname.h /usr/include/bits/getopt_posix.h \
- /usr/include/bits/getopt_core.h /usr/include/bits/unistd_ext.h \
- /usr/include/arpa/inet.h /usr/include/netinet/in.h \
- /usr/include/bits/stdint-uintn.h /usr/include/sys/socket.h \
- /usr/include/bits/types/struct_iovec.h /usr/include/bits/socket.h \
- /usr/include/bits/socket_type.h /usr/include/bits/sockaddr.h \
- /usr/include/asm/socket.h /usr/include/asm-generic/socket.h \
- /usr/include/linux/posix_types.h /usr/include/linux/stddef.h \
- /usr/include/asm/posix_types.h /usr/include/asm/posix_types_64.h \
- /usr/include/asm-generic/posix_types.h /usr/include/asm/bitsperlong.h \
- /usr/include/asm-generic/bitsperlong.h /usr/include/asm/sockios.h \
- /usr/include/asm-generic/sockios.h \
- /usr/include/bits/types/struct_osockaddr.h /usr/include/bits/in.h \
- /usr/include/SDL2/SDL.h /usr/include/SDL2/SDL_main.h \
- /usr/include/SDL2/SDL_stdinc.h /usr/include/SDL2/SDL_config.h \
- /usr/include/SDL2/SDL_platform.h /usr/include/SDL2/begin_code.h \
- /usr/include/SDL2/close_code.h /usr/include/SDL2/SDL_config_unix.h \
- /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/include/limits.h \
- /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/include/syslimits.h \
- /usr/include/limits.h /usr/include/bits/posix1_lim.h \
- /usr/include/bits/local_lim.h /usr/include/linux/limits.h \
- /usr/include/bits/pthread_stack_min-dynamic.h \
- /usr/include/bits/pthread_stack_min.h /usr/include/bits/posix2_lim.h \
+ /usr/include/alloca.h /usr/include/bits/stdlib-float.h \
+ /usr/include/string.h /usr/include/bits/types/locale_t.h \
+ /usr/include/bits/types/__locale_t.h /usr/include/strings.h \
  /usr/include/wchar.h /usr/include/bits/wchar.h \
  /usr/include/bits/types/wint_t.h /usr/include/bits/types/mbstate_t.h \
  /usr/include/inttypes.h \
  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/include/stdint.h \
- /usr/include/stdint.h /usr/include/bits/stdint-least.h \
- /usr/include/ctype.h /usr/include/math.h /usr/include/bits/math-vector.h \
+ /usr/include/stdint.h /usr/include/bits/stdint-uintn.h \
+ /usr/include/bits/stdint-least.h /usr/include/ctype.h \
+ /usr/include/math.h /usr/include/bits/math-vector.h \
  /usr/include/bits/libm-simd-decl-stubs.h \
  /usr/include/bits/flt-eval-method.h /usr/include/bits/fp-logb.h \
  /usr/include/bits/fp-fast.h /usr/include/bits/mathcalls-macros.h \
  /usr/include/bits/mathcalls-helper-functions.h \
  /usr/include/bits/mathcalls.h \
  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/include/float.h \
+ /usr/include/SDL2/SDL_rect.h /usr/include/SDL2/SDL_error.h \
+ /usr/include/SDL2/SDL_pixels.h /usr/include/SDL2/SDL_endian.h \
+ /usr/include/SDL2/SDL_rwops.h /usr/include/SDL2/SDL_video.h \
+ /usr/include/SDL2/SDL_surface.h /usr/include/SDL2/SDL_blendmode.h \
+ /usr/include/SDL2/SDL_timer.h /usr/include/unistd.h \
+ /usr/include/bits/posix_opt.h /usr/include/bits/environments.h \
+ /usr/include/bits/confname.h /usr/include/bits/getopt_posix.h \
+ /usr/include/bits/getopt_core.h /usr/include/bits/unistd_ext.h \
+ /usr/include/arpa/inet.h /usr/include/netinet/in.h \
+ /usr/include/sys/socket.h /usr/include/bits/types/struct_iovec.h \
+ /usr/include/bits/socket.h /usr/include/bits/socket_type.h \
+ /usr/include/bits/sockaddr.h /usr/include/asm/socket.h \
+ /usr/include/asm-generic/socket.h /usr/include/linux/posix_types.h \
+ /usr/include/linux/stddef.h /usr/include/asm/posix_types.h \
+ /usr/include/asm/posix_types_64.h /usr/include/asm-generic/posix_types.h \
+ /usr/include/asm/bitsperlong.h /usr/include/asm-generic/bitsperlong.h \
+ /usr/include/asm/sockios.h /usr/include/asm-generic/sockios.h \
+ /usr/include/bits/types/struct_osockaddr.h /usr/include/bits/in.h \
+ /usr/include/SDL2/SDL.h /usr/include/SDL2/SDL_main.h \
  /usr/include/SDL2/SDL_assert.h /usr/include/SDL2/SDL_atomic.h \
- /usr/include/SDL2/SDL_audio.h /usr/include/SDL2/SDL_error.h \
- /usr/include/SDL2/SDL_endian.h /usr/include/SDL2/SDL_mutex.h \
- /usr/include/SDL2/SDL_thread.h /usr/include/SDL2/SDL_rwops.h \
- /usr/include/SDL2/SDL_clipboard.h /usr/include/SDL2/SDL_cpuinfo.h \
+ /usr/include/SDL2/SDL_audio.h /usr/include/SDL2/SDL_mutex.h \
+ /usr/include/SDL2/SDL_thread.h /usr/include/SDL2/SDL_clipboard.h \
+ /usr/include/SDL2/SDL_cpuinfo.h \
  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/include/immintrin.h \
  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/include/x86gprintrin.h \
  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/include/ia32intrin.h \
@@ -175,20 +178,17 @@ simulator/CMakeFiles/Simulator.dir/src/simulator.c.o: \
  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/include/prfchwintrin.h \
  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/include/keylockerintrin.h \
  /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/include/amxfp16intrin.h \
- /usr/include/SDL2/SDL_events.h /usr/include/SDL2/SDL_video.h \
- /usr/include/SDL2/SDL_pixels.h /usr/include/SDL2/SDL_rect.h \
- /usr/include/SDL2/SDL_surface.h /usr/include/SDL2/SDL_blendmode.h \
- /usr/include/SDL2/SDL_keyboard.h /usr/include/SDL2/SDL_keycode.h \
- /usr/include/SDL2/SDL_scancode.h /usr/include/SDL2/SDL_mouse.h \
- /usr/include/SDL2/SDL_joystick.h /usr/include/SDL2/SDL_guid.h \
- /usr/include/SDL2/SDL_gamecontroller.h /usr/include/SDL2/SDL_sensor.h \
- /usr/include/SDL2/SDL_quit.h /usr/include/SDL2/SDL_gesture.h \
- /usr/include/SDL2/SDL_touch.h /usr/include/SDL2/SDL_filesystem.h \
- /usr/include/SDL2/SDL_haptic.h /usr/include/SDL2/SDL_hidapi.h \
- /usr/include/SDL2/SDL_hints.h /usr/include/SDL2/SDL_loadso.h \
- /usr/include/SDL2/SDL_log.h /usr/include/SDL2/SDL_messagebox.h \
- /usr/include/SDL2/SDL_metal.h /usr/include/SDL2/SDL_power.h \
- /usr/include/SDL2/SDL_render.h /usr/include/SDL2/SDL_shape.h \
- /usr/include/SDL2/SDL_system.h /usr/include/SDL2/SDL_timer.h \
- /usr/include/SDL2/SDL_version.h /usr/include/SDL2/SDL_locale.h \
- /usr/include/SDL2/SDL_misc.h
+ /usr/include/SDL2/SDL_events.h /usr/include/SDL2/SDL_keyboard.h \
+ /usr/include/SDL2/SDL_keycode.h /usr/include/SDL2/SDL_scancode.h \
+ /usr/include/SDL2/SDL_mouse.h /usr/include/SDL2/SDL_joystick.h \
+ /usr/include/SDL2/SDL_guid.h /usr/include/SDL2/SDL_gamecontroller.h \
+ /usr/include/SDL2/SDL_sensor.h /usr/include/SDL2/SDL_quit.h \
+ /usr/include/SDL2/SDL_gesture.h /usr/include/SDL2/SDL_touch.h \
+ /usr/include/SDL2/SDL_filesystem.h /usr/include/SDL2/SDL_haptic.h \
+ /usr/include/SDL2/SDL_hidapi.h /usr/include/SDL2/SDL_hints.h \
+ /usr/include/SDL2/SDL_loadso.h /usr/include/SDL2/SDL_log.h \
+ /usr/include/SDL2/SDL_messagebox.h /usr/include/SDL2/SDL_metal.h \
+ /usr/include/SDL2/SDL_power.h /usr/include/SDL2/SDL_render.h \
+ /usr/include/SDL2/SDL_shape.h /usr/include/SDL2/SDL_system.h \
+ /usr/include/SDL2/SDL_timer.h /usr/include/SDL2/SDL_version.h \
+ /usr/include/SDL2/SDL_locale.h /usr/include/SDL2/SDL_misc.h
--- simulator/src/simulator.c
+++ simulator/src/simulator.c
@@ -1,3 +1,5 @@
+#include <SDL2/SDL_render.h>
+#include <SDL2/SDL_timer.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,6 +8,25 @@
 #include <SDL2/SDL.h>
 
 #define PORT 8080
+
+const int SCREEN_WIDTH = 600;
+const int SCREEN_HEIGHT = 600;
+
+typedef struct{
+    SDL_Rect rect;
+    int vehicle_id;
+    char road_id;
+    int lane;
+    int speed;
+    char targetRoad;
+    int targetLane; 
+} Vehicle;
+
+void drawVehicle(SDL_Renderer *renderer, Vehicle *vehicle) {
+
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);  // Red vehicle
+    SDL_RenderFillRect(renderer, &vehicle->rect);
+}
 
 int InitializeSDL(void) {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -111,41 +132,93 @@ void DrawBackground(SDL_Renderer *renderer) {
     DrawTrafficLight(renderer, 395, 255, 1, "vertical");
     DrawTrafficLight(renderer, 255, 175, 0, "horizontal");
     DrawTrafficLight(renderer, 255, 395, 1, "horizontal");
-
-    SDL_RenderPresent(renderer);
-    SDL_Delay(16); 
+ 
 }
 
 
-typedef struct{
-    int vehicle_id;
-    char road_id;
-    int lane;
-} Vehicle;
-
-void receive_data(int sock) {
-    Vehicle received_data;
-
-    while (1) {
-        ssize_t bytes_received = recv(sock, &received_data, sizeof(received_data), 0);
-        
-        if (bytes_received == 0) {
-            printf("Server disconnected.\n");
-            break;
-        } else if (bytes_received < 0) {
-            perror("Receive failed");
-            break;
-        }
-        
-        printf("Received Data: Vehicle ID %d on Road %c Lane %cL%d\n",
-               received_data.vehicle_id, received_data.road_id, received_data.road_id, received_data.lane);
-        
-        sleep(1);
-    }
-}
+/*void receive_data(int sock) {*/
+/*    Vehicle received_data;*/
+/**/
+/*    while (1) {*/
+/*        ssize_t bytes_received = recv(sock, &received_data, sizeof(received_data), 0);*/
+/**/
+/*        if (bytes_received == 0) {*/
+/*            printf("Server disconnected.\n");*/
+/*            break;*/
+/*        } else if (bytes_received < 0) {*/
+/*            perror("Receive failed");*/
+/*            break;*/
+/*        }*/
+/**/
+/*        printf("Received Data: Vehicle ID %d on Road %c Lane %cL%d\n",*/
+/*               received_data.vehicle_id, received_data.road_id, received_data.road_id, received_data.lane);*/
+/**/
+/*        sleep(1);*/
+/*    }*/
+/*}*/
 
     static SDL_Window *window = NULL;
     static SDL_Renderer *renderer = NULL;
+
+    typedef struct{ 
+        int x_start, x_end;
+        int y_start, y_end;
+    } LanePosition;
+
+LanePosition lanePositions[4][3] = {
+    // A road lanes (A1, A2, A3)
+    { {150, 250, 0, 150}, {250, 350, 0, 150}, {350, 450, 0, 150} }, //check y_end imp!!!
+    
+    // B road lanes (B1, B2, B3)
+    {{350, 450, 450, 600}, {250, 350, 450, 600}, {150, 250, 450, 600}},
+    
+    // C road lanes (C1, C2, C3)
+    { {450, 600, 150, 250}, {450, 600, 250, 350}, {450, 600, 350, 450} },
+    
+    // D road lanes (D1, D2, D3)
+    {  {0, 150, 350, 450}, {0, 150, 250, 350},{0, 150, 150, 250} }
+};
+   
+void getLaneCenter(char road, int lane, int *x, int *y) {
+    int roadIndex = road - 'A';  // Convert 'A'-'D' to index 0-3
+    int laneIndex = lane - 1;    // Convert 1-3 to index 0-2
+
+    *x = (lanePositions[roadIndex][laneIndex].x_start + lanePositions[roadIndex][laneIndex].x_end) / 2 - 20 / 2;
+    *y = (lanePositions[roadIndex][laneIndex].y_start + lanePositions[roadIndex][laneIndex].y_end) / 2 - 20 / 2;
+
+    // Debugging output
+    printf("Vehicle starting at Road %c, Lane %d → Center (%d, %d)\n", road, lane, *x, *y);
+}
+
+void moveVehicle(Vehicle *vehicle) {
+    int targetX, targetY;
+    getLaneCenter(vehicle->targetRoad, vehicle->targetLane, &targetX, &targetY);
+
+    int reachedX = (abs(vehicle->rect.x - targetX) <= vehicle->speed);
+    int reachedY = (abs(vehicle->rect.y - targetY) <= vehicle->speed);
+
+    // 🌟 Fix: Decide movement priority based on road direction
+    if ((vehicle->road_id == 'A' && vehicle->targetRoad == 'C') || 
+        (vehicle->road_id == 'B' && vehicle->targetRoad == 'D')) {
+        // Move Y first (for A3 → C1 and B3 → D1)
+        if (!reachedY) {
+            if (vehicle->rect.y < targetY) vehicle->rect.y += vehicle->speed;  // Move down
+            else vehicle->rect.y -= vehicle->speed;  // Move up
+        } else if (!reachedX) {
+            if (vehicle->rect.x < targetX) vehicle->rect.x += vehicle->speed;  // Move right
+            else vehicle->rect.x -= vehicle->speed;  // Move left
+        }
+    } else {
+        // Default: Move X first (for D3 → A1 and C3 → B1)
+        if (!reachedX) {
+            if (vehicle->rect.x < targetX) vehicle->rect.x += vehicle->speed;  // Move right
+            else vehicle->rect.x -= vehicle->speed;  // Move left
+        } else if (!reachedY) {
+            if (vehicle->rect.y < targetY) vehicle->rect.y += vehicle->speed;  // Move down
+            else vehicle->rect.y -= vehicle->speed;  // Move up
+        }
+    }
+}
 
 int main() {
     // Socket related code commented out during the development of UI elements
@@ -154,7 +227,7 @@ int main() {
     if (InitializeSDL() < 0) {
         return 1;
     }
-    window = CreateWindow("Hello World", 600, 600);
+    window = CreateWindow("Traffic Simulator", SCREEN_WIDTH, SCREEN_HEIGHT);
     if (!window) {   
         return 1;
     }
@@ -165,6 +238,22 @@ int main() {
 
     // connect_to_server(sock, "127.0.0.1");
 
+Vehicle vehicle1 = {
+    {0, 0, 20, 20},  // Temporary position (updated below)
+    1, 'D', 3, 2,    // ID=1, starts at road 'D', lane 3, speed=3
+    'A', 1          // Target is road 'A', lane 1
+    };
+    getLaneCenter(vehicle1.road_id, vehicle1.lane, &vehicle1.rect.x, &vehicle1.rect.y);
+
+    Vehicle vehicle2 = {
+    {0, 0, 20, 20},  // Temporary values (updated below)
+    2, 'A', 3, 2,  // ID=2, starts at road 'A', lane 3
+    'C', 1                     // Target is road 'C', lane 1
+    };
+
+    // Set correct initial position (centered in A3)
+    getLaneCenter(vehicle2.road_id, vehicle2.lane, &vehicle2.rect.x, &vehicle2.rect.y);
+
     int running = 1;
     SDL_Event event;
     while (running) {
@@ -173,7 +262,16 @@ int main() {
                 running = 0;
             }
         }
+        moveVehicle(&vehicle1);
+        moveVehicle(&vehicle2);
         DrawBackground(renderer);
+
+        drawVehicle(renderer, &vehicle1);
+        drawVehicle(renderer, &vehicle2);
+
+        SDL_RenderPresent(renderer);
+        SDL_Delay(30);
+
     }
 
     // receive_data(sock);
