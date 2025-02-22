670ce047dd8a04f123a93031a9d264b49412efc0
Girish Chaulagain <girishchaulagain5@gmail.com>
2025-02-22 16:13:48 +0545

    Updated simulator.c to draw background

--- simulator/src/simulator.c
+++ simulator/src/simulator.c
@@ -14,3 +14,176 @@ int InitializeSDL(void) {
     }
     return 0;
 }
+
+SDL_Window* CreateWindow(const char *title, int width, int height) {
+    SDL_Window *window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_SHOWN | SDL_WINDOW_ALLOW_HIGHDPI);
+    if (!window) {
+        SDL_Log("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+    }
+    return window;
+}
+
+SDL_Renderer* CreateRenderer(SDL_Window *window) {
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        SDL_Log("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+    }
+    return renderer;
+}
+
+void DrawDashedLine(SDL_Renderer *renderer, int x1, int y1, int x2, int y2, int dashLength) {
+    int dx = x2 - x1;
+    int dy = y2 - y1;
+    int steps = (abs(dx) > abs(dy)) ? abs(dx) : abs(dy);
+    
+    float xIncrement = (float)dx / steps;
+    float yIncrement = (float)dy / steps;
+
+    float x = x1;
+    float y = y1;
+
+    for (int i = 0; i <= steps; i++) {
+        if ((i / dashLength) % 2 < 1) {
+            SDL_RenderDrawPoint(renderer, (int)x, (int)y);
+        }
+        x += xIncrement;
+        y += yIncrement;
+    }
+}
+
+void DrawTrafficLight(SDL_Renderer *renderer, int XPos, int YPos, int isGreen, char *orientation) {
+    
+    const int width = 30;
+    const int height = 90;
+
+    if (isGreen) {
+        SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+    } else {
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    }
+
+    SDL_Rect trafficLightRect;
+    
+    if (strcmp(orientation, "vertical") == 0) {
+        trafficLightRect = (SDL_Rect){XPos, YPos, width, height}; // Horizontal orientation
+    } else if (strcmp(orientation, "horizontal") == 0) {
+        trafficLightRect = (SDL_Rect){XPos, YPos, height, width}; // Vertical orientation
+    } else {
+        printf("Invalid orientation: %s\n", orientation);
+        return;
+    }
+    
+    SDL_RenderFillRect(renderer, &trafficLightRect);
+}
+
+void DrawBackground(SDL_Renderer *renderer) {
+    // Set background color (green for grass)
+    SDL_SetRenderDrawColor(renderer, 34, 139, 34, 255);
+    SDL_RenderClear(renderer);
+
+    // Set road color (gray)
+    SDL_SetRenderDrawColor(renderer, 50, 50, 50, 255);
+
+    // Horizontal road
+    SDL_Rect horizontalRoad = {0, 150, 600, 300}; 
+    SDL_RenderFillRect(renderer, &horizontalRoad);
+
+    // Vertical road
+    SDL_Rect verticalRoad = {150, 0, 300, 600}; 
+    SDL_RenderFillRect(renderer, &verticalRoad);
+
+    // Dashed lines for lane markings (yellow)
+    SDL_SetRenderDrawColor(renderer, 247, 233, 23, 255);
+    
+    DrawDashedLine(renderer, 0, 250, 150, 250, 10);
+    DrawDashedLine(renderer, 0, 350, 150, 350, 10);
+
+    DrawDashedLine(renderer, 450, 250, 600, 250, 10);
+    DrawDashedLine(renderer, 450, 350, 600, 350, 10);
+
+    DrawDashedLine(renderer, 250, 0, 250, 150, 10);
+    DrawDashedLine(renderer, 350, 0, 350, 150, 10);
+
+    DrawDashedLine(renderer, 250, 450, 250, 600, 10);
+    DrawDashedLine(renderer, 350, 450, 350, 600, 10);
+
+    DrawTrafficLight(renderer, 175, 255, 0, "vertical");
+    DrawTrafficLight(renderer, 395, 255, 1, "vertical");
+    DrawTrafficLight(renderer, 255, 175, 0, "horizontal");
+    DrawTrafficLight(renderer, 255, 395, 1, "horizontal");
+
+    SDL_RenderPresent(renderer);
+    SDL_Delay(16); 
+}
+
+
+typedef struct{
+    int vehicle_id;
+    char road_id;
+    int lane;
+} Vehicle;
+
+void receive_data(int sock) {
+    Vehicle received_data;
+
+    while (1) {
+        ssize_t bytes_received = recv(sock, &received_data, sizeof(received_data), 0);
+        
+        if (bytes_received == 0) {
+            printf("Server disconnected.\n");
+            break;
+        } else if (bytes_received < 0) {
+            perror("Receive failed");
+            break;
+        }
+        
+        printf("Received Data: Vehicle ID %d on Road %c Lane %cL%d\n",
+               received_data.vehicle_id, received_data.road_id, received_data.road_id, received_data.lane);
+        
+        sleep(1);
+    }
+}
+
+    static SDL_Window *window = NULL;
+    static SDL_Renderer *renderer = NULL;
+
+int main() {
+    // Socket related code commented out during the development of UI elements
+    // int sock = create_socket();
+
+    if (InitializeSDL() < 0) {
+        return 1;
+    }
+    window = CreateWindow("Hello World", 600, 600);
+    if (!window) {   
+        return 1;
+    }
+    renderer = CreateRenderer(window);
+    if (!renderer) {
+        return 1;
+    }
+
+    // connect_to_server(sock, "127.0.0.1");
+
+    int running = 1;
+    SDL_Event event;
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT || (event.type == SDL_KEYDOWN)) {
+                running = 0;
+            }
+        }
+        DrawBackground(renderer);
+    }
+
+    // receive_data(sock);
+
+    // Close socket
+    // close(sock);
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    
+    return 0;
+}
